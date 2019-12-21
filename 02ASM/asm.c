41a552afca6f34baf678ff2bfd76333d522f26d5
BinLewis <lybglory@outlook.com>
2019-12-21 21:23:15 +0800

    16进制打印地址

--- 02ASM/asm.c
+++ 02ASM/asm.c
@@ -3,7 +3,8 @@
 
 void main() {
 	int num = 5;
-	printf("num=%\d,address=%p\n",num,num);
+	printf("num=%\d,address=%x\n",num,num);
+	//%x 16进制打印地址
 	_asm {
 		mov eax,num //将变量num赋值给寄存器eax
 		add eax, 5	//寄存器+5
