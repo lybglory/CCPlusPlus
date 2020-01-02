7ede7607ae86747d036991ee5325ed5fffaab704
小斌仔 <lybglory@outlook.com>
2020-01-02 18:41:50 +0800

    三角输出

--- Day4/design.c
+++ Day4/design.c
@@ -75,8 +75,41 @@ void DoWhileFunc() {
 		b++;
 	}
 }
+/*
+#####
+ ####
+  ###
+   ##
+    #
+*/
+void ForFunc() {
+	int n = 5;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j <= n; j++)
+		{
+			printf("#");
+		}
+		printf("\n");
+	}
+	printf("========");
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (j>i) {
+				printf(" ");
+			}
+			else {
+				printf("#");
+			}
+		}
+		printf("\n");
+	}
+}
 void main() {
 	//ifElseIfElse();
 	//SanMuMatch();
-	DoWhileFunc();
+	//DoWhileFunc();
+	ForFunc();
 }
