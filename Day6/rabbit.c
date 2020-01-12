129cde76a99dad6a2be36456112aad48578fd676
BinLewis <lybglory@outlook.com>
2020-01-13 00:18:37 +0800

    递归实现进制转

--- Day6/rabbit.c
+++ Day6/rabbit.c
@@ -1,6 +1,5 @@
 #define  _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
 Rabbit(int x)
 {
 	int result;
@@ -11,10 +10,25 @@ Rabbit(int x)
 	}
 	return result;
 }
-
+int changedNum = 0;
+int ChangeSystem(int sysNum) {
+	if (sysNum==0) {
+		changedNum=0;
+	}
+	else {
+		int y = sysNum %2;
+		sysNum /= 2;
+		changedNum=ChangeSystem(sysNum);
+		printf("%d", y);
+	}
+	return changedNum;
+}
 void main() {
 	int num;
-	printf("请输入月份：");
+	//printf("请输入月份：");
+	//scanf("%d", &num);
+	//printf("%d个月后有 %d 个兔子", num, Rabbit(num));
+	printf("==10进制转二进制==\n请输入需要转换的数:");
 	scanf("%d", &num);
-	printf("%d个月后有 %d 个兔子", num, Rabbit(num));
+	ChangeSystem(num);
 }
