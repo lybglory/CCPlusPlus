eb9d7745c05f26821c4ea4c7b619b1e5f0a459a0
BinLewis <lybglory@outlook.com>
2020-01-19 23:06:40 +0800

    指针做函数参数
    数组名做函数参数，函数的形参会退化为指针

--- Day7/_04.c
+++ Day7/_04.c
@@ -1,12 +1,13 @@
 #define  _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-
-void printArrary(int arr[50],int n) {
-	//数组作为参数传递，传递的是元素的首地址。地址4个字节
-	printf("printArrary数组大小=%d\n",sizeof(arr));
+//数组名作为参数传递，传递的是元素的首地址。地址4个字节
+//void printArrary(int arr[50],int n) {
+void printArrary(int *arr, int n) {
+	//数组名做函数参数，函数的形参会退化为指针
+	printf("printArrary数组大小=%d\n",sizeof(arr));//4
 	for (size_t i = 0; i < n; i++)
 	{
-		printf("%d ",arr[i]);
+		printf("%d ",*arr+i);
 
 	}
 }
