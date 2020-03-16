32755c2921a7dd244599d3e74b797c07ccd37522
BinLewis <lybglory@outlook.com>
2020-03-16 22:53:03 +0800

    Simply define aliases

---
+++ 01_C_Foundation/D10_Standard_Files/D10_Standard_Files/main.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+typedef int intType;
+
+typedef struct {
+	char *name;
+	int age;
+	char sex;
+}PR;
+
+void tydefStru() {
+	//struct Person Messi = { "Messi",30,'M' };
+	PR Messi= { "Messi",30,'M' };
+	printf("%s %d %c\n",Messi.name,Messi.age,Messi.sex);
+}
+void TestTypedef() {
+	int n1 = 2020;
+	intType nt = 316;
+	printf("n1=%d; nt=%d\n",n1,nt);
+}
+void main() {
+	TestTypedef();
+}
