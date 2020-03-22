8440506dc653d5fc965ec20ccbc8d3a8050b19f4
BinLewis <lybglory@outlook.com>
2020-03-22 17:02:16 +0800

    fseek

--- 01_C_Foundation/D11_FilesOperations/11_02.c
+++ 01_C_Foundation/D11_FilesOperations/11_02.c
@@ -21,6 +21,28 @@ void Testrewind() {
 	}
 	fclose(fp);
 }
+
+void Testfseek() {
+	FILE *fp = fopen("note.txt","w+");
+	if (fp==NULL) {
+		perror("fopen error!");
+		return;
+	}
+
+	fputs("Date:2020/03/22 17:00\nToday is sunny day",fp);
+	//fclose(fp);
+	//rewind(fp);
+	fseek(fp,0, 0);
+	fp= fopen("note.txt", "r");
+	//fseek(fp, -18, SEEK_END);//"Today is sunny day"
+	fseek(fp, 5, SEEK_SET);
+	char chr[100] = ""; 
+	while (fgets(chr, sizeof(chr), fp)) {
+		printf("chr=%s", chr);
+	}
+	fclose(fp);
+}
 void main() {
-	Testrewind();
+	//Testrewind();
+	Testfseek();
 }
