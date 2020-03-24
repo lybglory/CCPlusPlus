bd0a5feb68b32f09067d1b218d75133f555f174c
小斌仔 <lybglory@outlook.com>
2020-03-24 16:50:10 +0800

    Draw the right wall(20 lines)

--- 01_C_Foundation/GluttonousSnake/GluttonousSnake/snake.c
+++ 01_C_Foundation/GluttonousSnake/GluttonousSnake/snake.c
@@ -5,12 +5,19 @@
 #include "snake.h"
 
 extern void InitWall() {
-	for (int x = 0; x <= Wide; x++){
-		if (x==Wide) {
-			printf("|");
+	//控制Y(第几行)
+	for (int y = 0; y <=Hight; y++){	
+
+		//控制x轴
+		for (int x = 0; x <= Wide; x++) {
+			if (x == Wide) {
+				printf("|");
+			}
+			else {
+				printf(" ");
+			}
 		}
-		else{
-			printf(" ");
-		}
+		printf("\n");
 	}
+	
 }
