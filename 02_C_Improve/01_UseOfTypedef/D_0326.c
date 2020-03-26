77d4f1ad1fd890facd3da1a83385f469230ddc9b
BinLewis <lybglory@outlook.com>
2020-03-27 00:19:04 +0800

    Modification of variables
    get Pointer value

--- 02_C_Improve/01_UseOfTypedef/D_0326.c
+++ 02_C_Improve/01_UseOfTypedef/D_0326.c
@@ -7,13 +7,15 @@ typedef struct {
 	int age;		//0~3
 	char name[4];	//4~7
 	char sex;		// 8~11
+	int money;		//12~15
 	
 }Person;
 void main() {
-	Person pr = { 32,"CG0",'M'};
+	Person pr = { 30,"LZZ",'M',90};
 	char *p = &pr;
-	printf("age=%d;  name=%s; sex=%c\n",pr.age,pr.name, pr.sex);
-	printf("&age=%x;&name=%x;s&ex=%x\n",&pr.age,pr.name,&pr.sex);
-	printf("*p+4=%s\n", (char *)(p + 4));
-	printf("*p+8=%c\n", *(char *)(p + 8));
+	printf("age=%d;  name=%s; sex=%c; money=%d\n",pr.age,pr.name, pr.sex,pr.money);
+	printf("&age=%x; &name=%x; &sex=%x;&money=%x\n",&pr.age,pr.name,&pr.sex,&pr.money);
+	printf("(p+4)=%s\n", (char *)(p + 4));
+	printf("(p+8)=%c\n", *(char *)(p + 8));
+	printf("(p+12)=%d\n", *(int *)(p + 12));
 }
