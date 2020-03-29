004b816c02fcafb05649e5d0e4060bee839ffb36
BinLewis <lybglory@outlook.com>
2020-03-29 22:34:34 +0800

    The macro function

--- 02_C_Improve/02_Pointer(advanced)/0329.c
+++ 02_C_Improve/02_Pointer(advanced)/0329.c
@@ -2,6 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void main() {
+#define Minus(a,b) a-b
 
+void MacroFun() {
+	int n1 = 2020;
+	int n2 = 2019;
+	int result = Minus(n1, n2);
+
+	printf("result=%d\n",result);
+}
+void main() {
+	MacroFun();
 }
