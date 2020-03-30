e0c48a526cfc71113fd6f48bdd719e1bb9419d6d
BinLewis <lybglory@outlook.com>
2020-03-30 22:59:38 +0800

    Pointer step size, get the string property

--- 02_C_Improve/02_Pointer(advanced)/0330_PointerStepLength.c
+++ 02_C_Improve/02_Pointer(advanced)/0330_PointerStepLength.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <stddef.h>
 void test01() {
 	char *p = NULL;
 	printf("p=%d\np+1=%d\n",p,p+1);
@@ -11,6 +11,22 @@ void test01() {
 	printf("dp=%d\ndp+1=%d\n", dp, dp + 1);
 
 }
+struct Person {
+	int age;		//0~3
+	char name[10];	//4~13
+	char sex;		//14~15
+	int money;		//16~19
+
+};
+void GetStructValue() {
+	struct Person pr = { 30,"Messi",'M',900 };
+	char *p = &pr;
+	printf("offset money=%d\n",offsetof(struct Person,money));
+	printf("money=%d\n", *(int *)(p + 16));
+	printf("sex=%c\n", *(char *)(p + offsetof(struct Person, sex)));
+	printf("name=%s\n", (char*)(p + offsetof(struct Person, name)));
+}
 void main() {
-	test01();
+	//test01();
+	GetStructValue();
 }
