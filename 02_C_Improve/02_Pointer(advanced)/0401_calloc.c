545b05388865051900123fc0a427d14bc6c07d0f
BinLewis <lybglory@outlook.com>
2020-04-01 22:18:44 +0800

    realloc

--- 02_C_Improve/02_Pointer(advanced)/0401_calloc.c
+++ 02_C_Improve/02_Pointer(advanced)/0401_calloc.c
@@ -13,6 +13,30 @@ void TestCalloc() {
 		p = NULL;
 	}
 }
+
+void TestRealloc() {
+	int *p = calloc(5, sizeof(int));
+	if (p==NULL) {
+		return;
+	}
+
+	for (size_t i = 0; i < 5; i++)
+	{
+		p[i] = i;
+		printf("before:p[%d]=%d\n", i, p[i]);
+	}
+	//
+	p = realloc(p,sizeof(int)*10);
+	for (size_t i = 0; i < 10; i++)
+	{
+		printf("after:p[%d]=%d\n", i, p[i]);
+	}
+	if (p != NULL) {
+		free(p);
+		p = NULL;
+	}
+}
 void main() {
-	TestCalloc();
+	//TestCalloc();
+	TestRealloc();
 }
