9fe44f50d045697f7a37b36ed3b1117ba66ccc77
BinLewis <lybglory@outlook.com>
2020-04-04 23:32:28 +0800

    Secondary pointer input feature

--- 02_C_Improve/02_Pointer(advanced)/0404_BitOperation.c
+++ 02_C_Improve/02_Pointer(advanced)/0404_BitOperation.c
@@ -8,6 +8,29 @@ void BitTest1() {
 	printf("ret = %d\n", a); //ret = 2
 	printf("ret = %d\n", b); //ret = 253
 }
+void PrintfArray(int **secPointer,int len) {
+	for (size_t i = 0; i < len; i++){
+		printf("*secPointer[%d]=%d\n",i, *secPointer[i]);
+	}
+}
+
+//heap area
+void SecondaryPointerInput() {
+	int **sec_p = malloc(sizeof(int) * 5);
+	memset(sec_p,0, sizeof(int) * 5);
+	int array[5] = { 2020,4,4,23,50 };
+	int len = sizeof(array) / sizeof(array[0]);
+	for (size_t i = 0; i < len; i++){
+		sec_p[i] = &array[i];
+		printf("sec_p[%d]=%d; &array[%d]=%d\n",i,sec_p[i],i,&array[i]);
+	}
+	PrintfArray(sec_p,len);
+	if (sec_p!=NULL) {
+		free(sec_p);
+		sec_p = NULL;
+	}
+}
 void main() {
-	BitTest1();
+	//BitTest1();
+	SecondaryPointerInput();
 }
