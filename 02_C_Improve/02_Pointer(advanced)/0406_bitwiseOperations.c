637b2c533a3b1ded5234e4b34175c16a2de8bd78
BinLewis <lybglory@outlook.com>
2020-04-07 00:45:48 +0800

    XOR
    exchange two number

--- 02_C_Improve/02_Pointer(advanced)/0406_bitwiseOperations.c
+++ 02_C_Improve/02_Pointer(advanced)/0406_bitwiseOperations.c
@@ -23,7 +23,29 @@ void BitwiseOr() {
 				//0111=7
 	printf("%d | %d = %d\n",n1,n2,n1|n2);
 }
+
+void XOR() {
+	int  n1 = 2020;
+	int n2 = 407;	//
+	printf("before:n1=%d n2=%d\n", n1, n2);
+	//1¡¢XOR exchange
+	n1 = n1 ^ n2;
+	n2 = n1 ^ n2;
+	n1 = n1 ^ n2;
+
+	//2¡¢
+	//n1 = n1 + n2;
+	//n2 = n1 - n2;
+	//n1 = n1 - n2;
+	//3¡¢
+	//int temp = n1;
+	//n1 = n2;
+	//n2 = temp;
+
+	printf("after :n1=%d n2=%d\n",n1,n2);
+}
 void main() {
 	//Parity();
-	BitwiseOr();
+	//BitwiseOr();
+	XOR();
 }
