4006ebe4afdd0b5c40e6d3dba1532166c57b40af
BinLewis <lybglory@outlook.com>
2020-04-06 23:55:26 +0800

    Second pointer case

--- 02_C_Improve/02_Pointer(advanced)/0405_NSFileHandle.c
+++ 02_C_Improve/02_Pointer(advanced)/0405_NSFileHandle.c
@@ -8,7 +8,7 @@ int WirteAndGetLine(FILE *fp,int lines) {
 		perror("Open error!");
 		return;
 	}
-	char *buff[] = {"清明时节雨纷纷，\n路上行人欲断魂。\n借问酒家何处有？\n牧童遥指杏花村。" };
+	char *buff[] = {"清明时节雨纷纷，\n路上行人欲断魂。\n借问酒家何处有？\n牧童遥指杏花村。\n" };
 	int counts=sizeof(buff)/sizeof(buff[0]); //lines=1
 	int i = 0;
 	while (i < counts) {
@@ -36,16 +36,15 @@ void WriteArray(FILE *fp,char **array,int len) {
 	char buffer[200] = {0};//字符串数组，要搞清楚。
 	while (fgets(buffer,sizeof(buffer), fp)!=NULL)
 	{
-		printf("buffer=%s", buffer);
-		int currentLineChars = strlen(buffer)+1;
-		printf("%d\n",currentLineChars);
+		int currBytes = strlen(buffer)+1;
 		int lastChar = strlen(buffer) - 1;
-		//字符串最后字符改为 \0
+		//字符串最后字符改为 \0,不换行
 		buffer[lastChar] = '\0';
-
-		int lineSize = sizeof(char) * currentLineChars;
+		printf("buffer=%s--%d\n", buffer,currBytes);
+		int lineSize = sizeof(char) * currBytes;
 		//heap area 每一行开辟一个堆区
-		char *lineStr = malloc(lineSize);
+		char *lineStr = (char *)malloc(lineSize);
+		memset(lineStr,0, lineSize);
 		strcpy(lineStr, buffer);
 		array[index++] = lineStr;
 		memset(buffer,0, sizeof(buffer));
@@ -57,7 +56,7 @@ void WriteArray(FILE *fp,char **array,int len) {
 void PrintfArr(char **pArray,int lines) {
 	for (size_t i = 0; i < lines; i++)
 	{
-		printf("%d line:%s\n", i, pArray[i]);
+		printf("%d line:%s\n", i+1, pArray[i]);
 	}
 }
 
@@ -67,7 +66,7 @@ void freeSpace(char** pArray, int len)
 	{
 		if (pArray[i] != NULL)
 		{
-			printf("%s被释放了\n", pArray[i]);
+			printf("%s has been freed\n", pArray[i]);
 			free(pArray[i]);
 			pArray[i] = NULL;
 		}
@@ -86,7 +85,7 @@ void main() {
 	int lines = 0;
 	lines = WirteAndGetLine(fp, lines);
 	printf("\nA-Row=%d\n", lines);
-	char **toWriteArr = (char *)malloc(sizeof(char)*lines);
+	char **toWriteArr = (char **)malloc(sizeof(char)*lines);
 	//memset(toWriteArr,0, sizeof(char) * lines);
 	WriteArray(fp, toWriteArr,lines);
 	PrintfArr(toWriteArr,lines);
