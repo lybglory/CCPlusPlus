36b71b421fbbd948dc0e8a31d9bb31f1f311eb9f
BinLewis <lybglory@outlook.com>
2020-04-07 23:32:46 +0800

    linear array name

---
+++ 02_C_Improve/04_ArraysAndStructs/0407_Array.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void LinearArray01() {
+	//
+	int array[5] = { 2016,2017,2018,2019,2020 };
+	printf("&array  =%d\n",&array);
+	printf("&array+1=%d\n", &array+1);
+}
+void main() {
+	LinearArray01();
+}
