6b47ec15cc68568a68bae10cd02253b5552b57c6
BinLewis <lybglory@outlook.com>
2020-04-08 00:07:34 +0800

    not used as a pointer constant
    Where the array name is not used as a pointer constant

--- 02_C_Improve/04_ArraysAndStructs/0407_Array.c
+++ 02_C_Improve/04_ArraysAndStructs/0407_Array.c
@@ -5,6 +5,8 @@
 void LinearArray01() {
 	//
 	int array[5] = { 2016,2017,2018,2019,2020 };
+	int* p = array;
+	printf("sizeof(array):%d\n", sizeof(array)); 	//此时sizeof结果为整个数组的长度
 	printf("&array  =%d\n",&array);
 	printf("&array+1=%d\n", &array+1);
 }
