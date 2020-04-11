e089ef45952945d7ba84c67fbe5f60b4be2d4a22
BinLewis <lybglory@outlook.com>
2020-04-11 16:18:43 +0800

    selection sort

--- 02_C_Improve/04_ArraysAndStructs/0411.c
+++ 02_C_Improve/04_ArraysAndStructs/0411.c
@@ -16,9 +16,25 @@ void NormalSort(int* p, int len) {
 	}
 }
 
+void SelectionSort(int *p,int len) {
+	for (int i = 0; i < len; i++)
+	{	//2020,2016,2019,2017,2018
+		int min = i;						//第一个下标
+		for (size_t j = i+1; j < len; j++)
+		{
+			if (p[min]> p[j]) {
+				min = j;
+			}
+		}
+		if (i!=min) {
+			int temp = p[i];
+			p[i] = p[min];
+			p[min] = temp;
+		}
+	}
+}
 
 void PrintfArray(int *arrP,int len) {
-	printf("\n");
 	for (size_t i = 0; i < len; i++)
 	{
 		printf("arrP[%d]=%d ",i,arrP[i]);
@@ -27,7 +43,7 @@ void PrintfArray(int *arrP,int len) {
 void main() {
 	int array[] = { 2020,2016,2019,2017,2018 };
 	int arrayLen = sizeof(array) / sizeof(array[0]);
-
-	NormalSort(array, arrayLen);
+	//NormalSort(array,arrayLen);
+	SelectionSort(array,arrayLen);
 	PrintfArray(array,arrayLen);
 }
