114378b176be3e4a802adebaa212687a89b6f923
BinLewis <lybglory@outlook.com>
2020-04-12 00:41:57 +0800

    String selection sort

--- 02_C_Improve/04_ArraysAndStructs/0411.c
+++ 02_C_Improve/04_ArraysAndStructs/0411.c
@@ -52,11 +52,44 @@ void PrintfArray(int *arrP,int len) {
 		printf("arrP[%d]=%d ",i,arrP[i]);
 	}
 }
+
+void PtArrSelectionSort(char *p[], int len) {
+	for (size_t i = 0; i < len; i++)
+	{
+		int min = i;
+		for (size_t j = i+1; j < len; j++)
+		{	//比较字符，比较的是ASCII码,如果>0,说明有更小的元素
+			if (strcmp(p[min], p[j])>0) {
+				min = j;
+			}
+		}
+		if (i!=min) {
+			char *temp = p[i];
+			p[i] = p[min];
+			p[min] = temp;
+		}
+	}
+
+}
+
+void PrintfPointerArr(char **p,int len) {
+	for (size_t i = 0; i < len; i++)
+	{
+		printf("p[%d]=%s\n", i, p[i]);
+	}
+}
+void PointerArr() {
+	char *str[] = {"Date:2020/4/12","Time:0:30","Missing glue pudding"};
+	int len = sizeof(str) / sizeof(char*);
+	PtArrSelectionSort(str,len);
+	PrintfPointerArr(str, len);
+}
 void main() {
-	int array[] = { 2020,2016,2019,2017,2018 };
-	int arrayLen = sizeof(array) / sizeof(array[0]);
+	//int array[] = { 2020,2016,2019,2017,2018 };
+	//int arrayLen = sizeof(array) / sizeof(array[0]);
 	//NormalSort(array,arrayLen);
-	BubbleSort(array, arrayLen);
+	//BubbleSort(array, arrayLen);
 	//SelectionSort(array,arrayLen);
-	PrintfArray(array,arrayLen);
+	//PrintfArray(array,arrayLen);
+	PointerArr();
 }
