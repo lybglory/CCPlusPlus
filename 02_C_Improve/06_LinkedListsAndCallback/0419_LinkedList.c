a558cdb05f5dbafe166c43cf8159b699f2449064
BinLewis <lybglory@outlook.com>
2020-04-20 23:23:52 +0800

    static linked list

--- 02_C_Improve/06_LinkedListsAndCallback/0419_LinkedList.c
+++ 02_C_Improve/06_LinkedListsAndCallback/0419_LinkedList.c
@@ -2,6 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void main() {
+struct LinkNode {
+	int year;
+	struct LinkNode *linkNext;
+};
+void StaticLinked() {
+	struct LinkNode node1 = { 2017,NULL };
+	struct LinkNode node2 = { 2018,NULL };
+	struct LinkNode node3 = { 2019,NULL };
+	struct LinkNode node4 = { 2020,NULL };
 
+	node1.linkNext = &node2;	//establish relationship
+	node2.linkNext = &node3;
+	node3.linkNext = &node4;
+	
+	struct LinkNode *pLink = &node1;
+	while (pLink!=NULL)
+	{
+		printf("link:year=%d\n",pLink->year);
+		pLink = pLink->linkNext;
+	}
+}
+void main() {
+	StaticLinked();
 }
