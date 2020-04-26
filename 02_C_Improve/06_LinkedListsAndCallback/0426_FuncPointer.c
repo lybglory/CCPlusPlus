6c8e7f4dfed80bf1e69510f1edeffaca8d230c1e
小斌仔 <lybglory@outlook.com>
2020-04-26 15:21:40 +0800

    function pointer defined 2

--- 02_C_Improve/06_LinkedListsAndCallback/0426_FuncPointer.c
+++ 02_C_Improve/06_LinkedListsAndCallback/0426_FuncPointer.c
@@ -7,14 +7,24 @@ void Greeting(char *date,char *dailogue) {
 }
 
 void FuncPointerDefined1() {
+	//
 	typedef void(*pFunc)(char *date, char *dailogue);
-	char *p1 = "2020/04/26";
+	char *p1 = "defined1:2020/04/26";
 	char *p2 = "You are booked on flight numberCX167";
+	//
 	pFunc pfunc1 = Greeting;
 	pfunc1(p1, p2);
 }
 
+void FuncPointerDefined2() {
+	typedef void(pFunc)(char* date, char* dailogue);
+	char* p1 = "deinfed2:2020/04/26";
+	char* p2 = "I would like to reserve a round-trip ticket to Parise,please";
+	pFunc *pfunc1 = Greeting;
+	pfunc1(p1, p2);
+}
 void main() {
-	FuncPointerDefined1();
+	//FuncPointerDefined1();
+	FuncPointerDefined2();
 }
 
