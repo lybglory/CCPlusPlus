21b0ab77f7219cbb370fd5505ddb223a225358cd
小斌仔 <lybglory@outlook.com>
2020-04-28 10:41:00 +0800

    printf each element of any array(struct array)

--- 02_C_Improve/06_LinkedListsAndCallback/0427_CallbackFunc.c
+++ 02_C_Improve/06_LinkedListsAndCallback/0427_CallbackFunc.c
@@ -18,12 +18,17 @@ void CallbackArr(void(* pFunc)(void *),void(* data),int length,int eleSize) {
 }
 void PrintfIntArr(void(* data)) {
 	int *num = data;
-	printf("IntArr:%d\n",*num);
+	printf("IntArr: %d\n",*num);
 }
-
+void PrintfStrucArr(void *data) {
+	struct Person *pr = (struct Person *)data;
+	printf("person: name=%s; age=%d\n",pr->name,pr->age);
+}
 void main() {
 	int yearArr[5] = {2020,2019,2018,2017,2016};
 	int yearLeng = sizeof(yearArr) / sizeof(yearArr[0]);
 	int eleSize = sizeof(yearArr[0]);
 	CallbackArr(PrintfIntArr,yearArr, yearLeng, eleSize);
+	struct Person pr[2] = { {"Messi",33},{"bin",30} };
+	CallbackArr(PrintfStrucArr, pr, sizeof(pr) / sizeof(pr[0]), sizeof(pr[0]));
 }
