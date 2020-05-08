0ff55d54e4837ad16c4e06044cd1010df5c8f4a2
BinLewis <lybglory@outlook.com>
2020-05-08 23:41:14 +0800

    remove dynamic  element of array by value

--- 02_C_Improve/08_DynamicArrayAndLinkList/0506_DynamicArrStruInit.c
+++ 02_C_Improve/08_DynamicArrayAndLinkList/0506_DynamicArrStruInit.c
@@ -84,25 +84,58 @@ void RemoveDyArrByPos(struct DyArrStru *dyArr,int pos) {
 	}
 	dyArr->arrCount--;//change element count
 }
+
+void RemoveDyArrByValue(struct DyArrStru *dyArr, void *data,int(* calbkCompare)(void *,void *)) {
+	if (dyArr==NULL || data==NULL) {
+		return;
+	}
+	for (int i = 0; i < dyArr->arrCount; i++)
+	{
+		if (calbkCompare(dyArr->dyptAddr[i],data)==1) {
+			RemoveDyArrByPos(dyArr, i);
+			break;
+		}
+	}
+}
+
+int CalbkCompare(void *data1,void *data2) {
+	Person *p1 = (Person *)data1;
+	Person *p2 = (Person *)data2;
+	if (strcmp(p1->name,p2->name)==0&&p1->age==p2->age) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
 void DyArrTest() {
 	struct DyArrStru *dyArr =DyArrInit(3);
 	Person p1 = { "Messi",33 };
 	Person p2 = { "LvBu",35 };
 	Person p3 = { "Bin",18 };
 	Person p4 = { "Mei",18 };
-	printf("before:Capacity=%d	count=%d\n",dyArr->arrCapacity,dyArr->arrCount);
+	printf("before:		Capacity=%d	count=%d\n",dyArr->arrCapacity,dyArr->arrCount);
 	InsertArr(dyArr, 1, &p1);
 	InsertArr(dyArr, 2, &p2);
 	InsertArr(dyArr, 0, &p3);
 	InsertArr(dyArr, 0, &p4);
 	//"Mei" "Bin" "Messi" "LvBu"
 	PrintDyArrComm(dyArr, CallbkDyPrint);
-	printf("after :Capacity=%d	count=%d\n", dyArr->arrCapacity, dyArr->arrCount);
+	printf("after:		Capacity=%d	count=%d\n", dyArr->arrCapacity, dyArr->arrCount);
+
+	//remove by position
 	RemoveDyArrByPos(dyArr, 3);	//remove "LvBu"
-	printf("\nremove:Capacity=%d	count=%d\n", dyArr->arrCapacity, dyArr->arrCount);
+	printf("\nremove by position:Capacity=%d	count=%d\n", dyArr->arrCapacity, dyArr->arrCount);
+	PrintDyArrComm(dyArr, CallbkDyPrint);
+
+	//remove by value
+	Person pr = { "Messi",33 };
+	RemoveDyArrByValue(dyArr,&pr,CalbkCompare);//remove "Messi"
+	printf("\nremove by value:Capacity=%d	count=%d\n", dyArr->arrCapacity, dyArr->arrCount);
 	PrintDyArrComm(dyArr, CallbkDyPrint);
 
 
+	//remove by value
 
 }
 
