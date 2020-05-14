7566f29e1b3209078e6875c0e96ab41941dcfab9
BinLewis <lybglory@outlook.com>
2020-05-15 00:38:43 +0800

    insert link list(enterprise)

--- 02_C_Improve/09_EnterpriseLinkAndStacks/0514_SinglyLink.c
+++ 02_C_Improve/09_EnterpriseLinkAndStacks/0514_SinglyLink.c
@@ -31,7 +31,50 @@ v_etpLink InitEnterpriseLinkls() {
 	return lkls;
 }
 
+void InsertLknd(v_etpLink v_lkls,void *insertData,int pos) {
+	if (v_lkls==NULL||insertData==NULL) {
+		return;
+	}
+	struct Linkls *lkls = v_lkls;
+	struct Linknd *inSertNd = insertData;
+	struct Linknd *lkndCurr = lkls->lklsHead;
 
+	//max position=lkndCount-1
+	if (pos<0|| pos>lkls->lkndCount-1) {
+		pos = lkls->lkndCount;
+	}
+	//find precursor node
+	for (int i = 0; i < lkls->lkndCount; i++)
+	{
+		lkndCurr = lkndCurr->lkndNext;
+	}
+	inSertNd->lkndNext = lkndCurr->lkndNext;
+	lkndCurr->lkndNext = inSertNd;
+	lkls->lkndCount--;
+}
+
+void TraversalLinkls(v_etpLink v_lkls, void(* calbk)(void *)) {
+	if (v_lkls==NULL) {
+		return;
+	}
+	struct Linkls *lkls= v_lkls;
+	for (int i = 0; i < lkls->lkndCount; i++)
+	{
+
+	}
+
+}
+void TestEtpLinkls() {
+	PersonInfo p1 = {NULL,"Soup",1};
+	PersonInfo p2 = { NULL,"Bin",18 };
+	PersonInfo p3 = { NULL,"Mei",16 };
+	PersonInfo p4 = { NULL,"Chan",15 };
+	v_etpLink lkls = InitEnterpriseLinkls();
+	InsertLknd(lkls,&p1,3);
+	InsertLknd(lkls, &p2, 0);
+	InsertLknd(lkls, &p3, 1);
+	InsertLknd(lkls, &p4, 2);
+}
 void main() {
 
 }
