96bd6b8c946e945c556e44a8a9583a9a6025f9f0
小斌仔 <lybglory@outlook.com>
2020-05-15 17:37:28 +0800

    Fix the insertion location problem

--- 02_C_Improve/09_EnterpriseLinkAndStacks/0514_SinglyLink.c
+++ 02_C_Improve/09_EnterpriseLinkAndStacks/0514_SinglyLink.c
@@ -46,7 +46,7 @@ void InsertLknd(v_etpLink v_lkls,void *insertData,int pos) {
 		pos = lkls->lkndCount;
 	}
 	//find precursor node
-	for (int i = 0; i < lkls->lkndCount; i++)
+	for (int i = 0; i < pos; i++)
 	{
 		lkndCurr = lkndCurr->lkndNext;
 	}
@@ -73,20 +73,43 @@ void CalbkPrinPerson(void *data) {
 	printf("%s	%d\n",pData->name,pData->age);
 }
 
+void RemoveLinkNd(v_etpLink v_lkls,int pos) {
+	if (NULL == v_lkls) {
+		return;
+	}
+	struct Linkls *lkls= v_lkls;
+	if (pos<0||pos>lkls->lkndCount-1) {	//Invalid position
+		return;		
+	}
+
+	struct Linknd *lkndCurr = &lkls->lklsHead;
+	for (int i = 0; i < pos; i++)		//find precursor node
+	{
+		lkndCurr = lkndCurr->lkndNext;
+	}
+	struct Linknd *lkndDel = lkndCurr->lkndNext;
+	lkndCurr->lkndNext = lkndDel->lkndNext;
+	lkls->lkndCount--;
+
+}
 
 void TestEtpLinkls() {
 	PersonInfo p1 = {NULL,"Soup",1};
 	PersonInfo p2 = { NULL,"Bin",18 };
 	PersonInfo p3 = { NULL,"Mei",16 };
 	PersonInfo p4 = { NULL,"Chan",15 };
+	PersonInfo p5 = { NULL,"Juan",29 };
 	v_etpLink v_lkls = InitEnterpriseLinkls();
-	InsertLknd(v_lkls,&p1,3);
-	InsertLknd(v_lkls, &p2, 0);
-	InsertLknd(v_lkls, &p3, 1);
-	InsertLknd(v_lkls, &p4, 2);
+	InsertLknd(v_lkls, &p1,3);	//Soup
+	InsertLknd(v_lkls, &p2, 0);	//Bin Soup
+	InsertLknd(v_lkls, &p3, 1);	//Bin Mei Soup
+	InsertLknd(v_lkls, &p4, 2);	//Bin Mei Chan Soup
+	InsertLknd(v_lkls, &p5, 0);	//Juan Bin Mei Chan Soup
 	TraversalLinkls(v_lkls,CalbkPrinPerson);
 
-
+	printf("Remove by pos 0\n");
+	RemoveLinkNd(v_lkls,0);
+	TraversalLinkls(v_lkls, CalbkPrinPerson);
 }
 void main() {
 	TestEtpLinkls();
