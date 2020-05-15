7261c43b540ba0e08776fc11826887d8d14d4164
小斌仔 <lybglory@outlook.com>
2020-05-15 16:32:17 +0800

    traversal link list

--- 02_C_Improve/09_EnterpriseLinkAndStacks/0514_SinglyLink.c
+++ 02_C_Improve/09_EnterpriseLinkAndStacks/0514_SinglyLink.c
@@ -2,14 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-struct Linkls {
-	struct Linknd *lklsHead;
-	int lkndCount;
-};
 //node just record pointer arear
 struct Linknd {
 	struct Linknd *lkndNext;
 };
+
+struct Linkls {
+	struct Linknd lklsHead;
+	int lkndCount;
+};
+
 
 typedef struct {
 	int *pAddr;	//Any four-byte data type is ok
@@ -27,7 +29,7 @@ v_etpLink InitEnterpriseLinkls() {
 		return;
 	}
 	lkls->lkndCount = 0;
-	lkls->lklsHead->lkndNext = NULL;	//valid data
+	lkls->lklsHead.lkndNext = NULL;
 	return lkls;
 }
 
@@ -37,7 +39,7 @@ void InsertLknd(v_etpLink v_lkls,void *insertData,int pos) {
 	}
 	struct Linkls *lkls = v_lkls;
 	struct Linknd *inSertNd = insertData;
-	struct Linknd *lkndCurr = lkls->lklsHead;
+	struct Linknd *lkndCurr = &lkls->lklsHead;
 
 	//max position=lkndCount-1
 	if (pos<0|| pos>lkls->lkndCount-1) {
@@ -50,7 +52,7 @@ void InsertLknd(v_etpLink v_lkls,void *insertData,int pos) {
 	}
 	inSertNd->lkndNext = lkndCurr->lkndNext;
 	lkndCurr->lkndNext = inSertNd;
-	lkls->lkndCount--;
+	lkls->lkndCount++;
 }
 
 void TraversalLinkls(v_etpLink v_lkls, void(* calbk)(void *)) {
@@ -58,23 +60,34 @@ void TraversalLinkls(v_etpLink v_lkls, void(* calbk)(void *)) {
 		return;
 	}
 	struct Linkls *lkls= v_lkls;
+	struct Linknd *lkndCurr = lkls->lklsHead.lkndNext;
 	for (int i = 0; i < lkls->lkndCount; i++)
 	{
-
+		calbk(lkndCurr);
+		lkndCurr = lkndCurr->lkndNext;
 	}
-
 }
+
+void CalbkPrinPerson(void *data) {
+	PersonInfo *pData = data;
+	printf("%s	%d\n",pData->name,pData->age);
+}
+
+
 void TestEtpLinkls() {
 	PersonInfo p1 = {NULL,"Soup",1};
 	PersonInfo p2 = { NULL,"Bin",18 };
 	PersonInfo p3 = { NULL,"Mei",16 };
 	PersonInfo p4 = { NULL,"Chan",15 };
-	v_etpLink lkls = InitEnterpriseLinkls();
-	InsertLknd(lkls,&p1,3);
-	InsertLknd(lkls, &p2, 0);
-	InsertLknd(lkls, &p3, 1);
-	InsertLknd(lkls, &p4, 2);
+	v_etpLink v_lkls = InitEnterpriseLinkls();
+	InsertLknd(v_lkls,&p1,3);
+	InsertLknd(v_lkls, &p2, 0);
+	InsertLknd(v_lkls, &p3, 1);
+	InsertLknd(v_lkls, &p4, 2);
+	TraversalLinkls(v_lkls,CalbkPrinPerson);
+
+
 }
 void main() {
-
+	TestEtpLinkls();
 }
