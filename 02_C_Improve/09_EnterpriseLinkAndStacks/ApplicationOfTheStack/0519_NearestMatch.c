6f758a3925cbe266ecff075e0c05a7395173a1d1
BinLewis <lybglory@outlook.com>
2020-05-20 22:09:04 +0800

    Stack application case - nearest matching(left matching)

--- 02_C_Improve/09_EnterpriseLinkAndStacks/ApplicationOfTheStack/0519_NearestMatch.c
+++ 02_C_Improve/09_EnterpriseLinkAndStacks/ApplicationOfTheStack/0519_NearestMatch.c
@@ -31,8 +31,58 @@ void TestSeqStk() {
 	printf("Pop after count=%d\n\n", GetSeqStkCount(vdSeqStk));
 
 	DestorySeqStk(vdSeqStk);
-
 }
+
+int GetLRBracket(char chr) {
+	if (chr=='(') {
+		return 9;
+	}
+	else if (chr==')') {
+		return 0;
+	}
+	else
+	{
+		return -1;
+	}
+}
+
+void ShowErrorMsg(char *arith,char *msg,char *chr) {
+	printf("error:%s\n",msg);
+	printf("%s\n",arith);
+	int chrPos =  chr-arith;
+	for (int i = 0; i < chrPos; i++)
+	{
+		printf(" ");
+	}
+	printf("^\n");
+}
+void StkMatch() {
+	char *arithmetic= "2020/(5/20)-(21*50)+(3))";
+	char *p = arithmetic;
+	VdSequenStk vdSeqStk = SeqStkInit();
+	while (*p !='\0')
+	{	//left bracket,return 9.Push
+		if (GetLRBracket(*p) == 9) {
+			PushSeqStk(vdSeqStk, *p);
+		}
+		//right bracket,return 0.Pop
+		if (GetLRBracket(*p)==0) {
+			if (GetSeqStkCount(vdSeqStk) > 0) {
+				PopSeqStk(vdSeqStk);
+			}
+			else
+			{
+				ShowErrorMsg(arithmetic, "right bracket matching failed!", p);//is not *p
+				break;
+			}
+		}
+		p++;
+	}//while_end
+
+	
+}//func_end
+
 void main() {
-	TestSeqStk();
+	//TestSeqStk();
+	StkMatch();
 }
