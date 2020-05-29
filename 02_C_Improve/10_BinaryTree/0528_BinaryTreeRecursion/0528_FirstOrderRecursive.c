7a2a1bf6e1d6538f8758c823e156461aea3fa56a
小斌仔 <lybglory@outlook.com>
2020-05-29 15:03:10 +0800

    the first order

--- 02_C_Improve/10_BinaryTree/0528_BinaryTreeRecursion/0528_FirstOrderRecursive.c
+++ 02_C_Improve/10_BinaryTree/0528_BinaryTreeRecursion/0528_FirstOrderRecursive.c
@@ -2,7 +2,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+typedef struct{
+	char btName;
+	struct BinaryTreeNode *lChildNd;
+	struct BinaryTreeNode *rChildNd;
+}BinaryTreeNode;
 
+
+void BinaryTreeRecursion(BinaryTreeNode *btRoot) {
+	
+	if (btRoot ==NULL) {
+		return;
+	}
+	//ÏÈÐò
+	printf("%c ",btRoot->btName);			//1.root name
+	BinaryTreeRecursion(btRoot->lChildNd);	//2.left
+	BinaryTreeRecursion(btRoot->rChildNd);	//3.right
+
+
+}
+
+void BinaryTreeTest() {
+	BinaryTreeNode btNodeA = { 'A',NULL,NULL };
+	BinaryTreeNode btNodeB = { 'B',NULL,NULL };
+	BinaryTreeNode btNodeC = { 'C',NULL,NULL };
+	BinaryTreeNode btNodeD = { 'D',NULL,NULL };
+	BinaryTreeNode btNodeE = { 'E',NULL,NULL };
+	BinaryTreeNode btNodeF = { 'F',NULL,NULL };
+	BinaryTreeNode btNodeG = { 'G',NULL,NULL };
+	BinaryTreeNode btNodeH = { 'H',NULL,NULL };
+	btNodeA.lChildNd = &btNodeB;
+	btNodeA.rChildNd = &btNodeF;
+
+	btNodeB.rChildNd = &btNodeC;
+
+	btNodeC.lChildNd = &btNodeD;
+	btNodeC.rChildNd = &btNodeE;
+
+	btNodeF.rChildNd = &btNodeG;
+
+	btNodeG.lChildNd = &btNodeH;
+
+	BinaryTreeRecursion(&btNodeA);
+}
 void main() {
-
+	BinaryTreeTest();
 }
