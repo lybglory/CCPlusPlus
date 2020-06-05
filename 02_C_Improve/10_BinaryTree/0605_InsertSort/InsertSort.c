f825a3dcb09d65fdf2b380bf22e7ae475865fe12
小斌仔 <lybglory@outlook.com>
2020-06-05 19:10:24 +0800

    fixed insert sort error

--- 02_C_Improve/10_BinaryTree/0605_InsertSort/InsertSort.c
+++ 02_C_Improve/10_BinaryTree/0605_InsertSort/InsertSort.c
@@ -4,17 +4,18 @@
 #include <string.h>
 
 void InsertSort(int *arr,int length) {
-	for (int i = 0; i < length; i++)
+	for (int i = 1; i < length; i++)
 	{	//前后比较。后面的数更小时
 		if (arr[i-1]>arr[i]) {
 			int temp = arr[i];
-			int j;
+			int j=i-1;
 
 			//for循环还得进行一次判断。
-			for ( j=0; j>=0&&arr[j]>temp; j++)
+			for ( ; j>=0 && arr[j]>temp; j--)
 			{
 				arr[j + 1] = arr[j];//更大的数往后移
 			}
+			arr[j + 1] = temp;		//原先更大的空出了空间，更小的值插入
 		}
 
 	}//for_i_end
@@ -36,5 +37,11 @@ void CalbkIntArr(void *element) {
 void main() {
 	int numArr[] = {2009,2006,2012,2020,2018,2015,2016,2019};
 	int arrLen = sizeof(numArr) / sizeof(numArr[0]);
+	printf("before: ");
 	IterateArrComm(numArr,CalbkIntArr,arrLen,sizeof(int));
+
+	InsertSort(numArr,arrLen);
+	printf("\nafter : ");
+	IterateArrComm(numArr, CalbkIntArr, arrLen, sizeof(int));
+	printf("\n");
 }
