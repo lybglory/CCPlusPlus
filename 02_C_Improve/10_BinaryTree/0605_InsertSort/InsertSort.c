08e9a74be4bfe8c9b6de47720a51d9e8d8dce575
小斌仔 <lybglory@outlook.com>
2020-06-05 16:34:39 +0800

    callback funcion

--- 02_C_Improve/10_BinaryTree/0605_InsertSort/InsertSort.c
+++ 02_C_Improve/10_BinaryTree/0605_InsertSort/InsertSort.c
@@ -20,6 +20,21 @@ void InsertSort(int *arr,int length) {
 	}//for_i_end
 }
 
+void IterateArrComm(void *arr,void( *calbkIterate)(void *),int length,int eleSize) {
+	char *pArr = arr;
+	for (int i = 0; i < length; i++)
+	{
+		char *temAddr = pArr + eleSize * i;//得到元素的地址
+		calbkIterate(temAddr);
+	}
+}
+
+void CalbkIntArr(void *element) {
+	int *n = (int *)element;
+	printf("%d ",*n);
+}
 void main() {
-	int numArr[] = {2009,2006,2012};
+	int numArr[] = {2009,2006,2012,2020,2018,2015,2016,2019};
+	int arrLen = sizeof(numArr) / sizeof(numArr[0]);
+	IterateArrComm(numArr,CalbkIntArr,arrLen,sizeof(int));
 }
