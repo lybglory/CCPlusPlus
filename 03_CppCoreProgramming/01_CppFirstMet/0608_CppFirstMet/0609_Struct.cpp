c77dda46cb98e23433adfcd62c280f6813ab0fb1
小斌仔 <lybglory@outlook.com>
2020-06-10 16:49:52 +0800

    The ternary enhancement

--- 03_CppCoreProgramming/01_CppFirstMet/0608_CppFirstMet/0609_Struct.cpp
+++ 03_CppCoreProgramming/01_CppFirstMet/0608_CppFirstMet/0609_Struct.cpp
@@ -10,6 +10,24 @@ struct Person {
 		cout << age << endl;
 	}
 };
-void main() {
+
+
+void TestStruct() {
 	Person p1 = { 18,"Chan" };	//2.c++中定义结构体变量不需要加struct关键字
+}
+
+void TestTernary() {
+	int a = 10;
+	int b = 20;
+	printf("ret:%d\n", a > b ? a : b);
+
+	cout << "b:" << b << endl;
+	//返回的是左值，变量的引用
+	(a > b ? a : b) = 100;		//返回的是左值，变量的引用
+	cout << "b:" << b << endl;
+
+}
+void main() {
+	//TestStruct();
+	TestTernary();
 }
