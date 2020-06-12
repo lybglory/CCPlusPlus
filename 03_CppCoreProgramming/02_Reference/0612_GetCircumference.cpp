c501a8d4ed17d6bc1b2a6f6f3d453689827cdb39
小斌仔 <lybglory@outlook.com>
2020-06-12 16:42:57 +0800

    Design a circle class

---
+++ 03_CppCoreProgramming/02_Reference/0612_GetCircumference.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECRUE_NO_WARING
+#include <iostream>
+using namespace std;
+const double pi = 3.14;
+class Circle {
+	
+public:
+	int r;
+	void SetR(int n) {
+		r = n;
+	}
+	double GetCircumference() {
+		return 2*pi * r;
+	}
+	int getR(){
+		return r;
+	}
+
+};
+
+
+void TestCircumference() {
+	Circle c1;
+	c1.SetR(10);
+	cout << "Circumference=" << c1.GetCircumference() << endl;
+}
+
+void main() {
+	TestCircumference();
+}
