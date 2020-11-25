dc1b0f26dec18d52e5e6b4936ad3bc927644dbfc
BinLewis <lybglory@outlook.com>
2020-11-25 23:26:47 +0800

    daffodil

---
+++ 03_CppCoreProgramming/05_OperaOverloading/Daffodil_20201125.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+void main() {
+	int num = 100;
+	do
+	{
+		int g = num % 10;
+		int s = num/10%10;
+		int b = num/100;
+		if (g * g * g + s * s * s + b * b * b == num) {//ÊÇdaffodil
+			cout << num << endl;
+		}
+		num++;
+
+	} while (num < 1000);
+}
