bf924e58ea0c3a1ab68ee8b7cc76607e0a09272b
小斌仔 <lybglory@outlook.com>
2021-04-09 16:38:51 +0800

    友元访问

--- 03_CppCoreProgramming/05_OperaOverloading/20210409_左移运算符重载/OverrideLeft.h
+++ 03_CppCoreProgramming/05_OperaOverloading/20210409_左移运算符重载/OverrideLeft.h
@@ -4,9 +4,10 @@ using namespace std;
 #pragma once
 
 class Person {
-
+	friend	ostream &operator<<(ostream &cout, Person &p);
 public:
 	Person(int age, int id);
+private:
 	int m_age;
 	int m_ID;
 };
