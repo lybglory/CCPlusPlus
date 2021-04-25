6592221153ec5f14060c42634a0a6b195d79b54f
Bin <lybglory@outlook.com>
2021-04-25 23:13:02 +0800

    智能指针

---
+++ 03_CppCoreProgramming/05_OperaOverloading/09_智能指针/SmartPoiny_20210425.cpp
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+class Person
+{
+public:
+	Person(int age) {
+		m_age = age;
+		cout << "有参构造：m_age="<<m_age << endl;
+	};
+
+	void ShowAge() {
+		cout << "ShowAge()_age="<<m_age << endl;
+	}
+	~Person() {
+		cout << "~Person()" << endl;
+	};
+
+private:
+	int m_age;
+};
+
+
+class Smartpt {
+
+public:
+	Smartpt(Person *p) {
+		cout << "Smartpt有参构造" << endl;
+		this->m_person = p;
+	}
+
+
+	~Smartpt() {
+		cout << "~Smartpt()" << endl;
+		if (this->m_person != NULL) {
+			delete this->m_person;
+			this->m_person = NULL;
+		}
+	}
+	Person *m_person;
+};
+
+
+
+void Test() {
+	Person *p = new Person(18);
+	p->ShowAge();
+
+	Smartpt smpt(new Person(30));
+	
+}
+
+void main() {
+	Test();
+}
