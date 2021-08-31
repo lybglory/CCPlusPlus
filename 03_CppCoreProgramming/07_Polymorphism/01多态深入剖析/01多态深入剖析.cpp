f6225311942c34adef49a232298e5bbb587a4090
Bin <lybglory@outlook.com>
2021-08-31 23:46:49 +0800

    polymorphism

---
+++ 03_CppCoreProgramming/07_Polymorphism/01多态深入剖析/01多态深入剖析.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+	 virtual void Speak() {
+		cout << "Anmail try to speak" << endl;
+	}
+};
+
+class Cat :public Animal{
+public:
+	void Speak() {
+		cout << "Cat try to speak" << endl;
+	}
+};
+
+class Dog :public Animal {
+public:
+	void Speak() {
+		cout << "Dog try to speak" << endl;
+	}
+};
+
+//父类的引用指向子类对象
+void DoSpeak(Animal &a) {
+	a.Speak();
+}
+
+void main() {
+	Cat c1;
+	DoSpeak(c1);
+
+}
