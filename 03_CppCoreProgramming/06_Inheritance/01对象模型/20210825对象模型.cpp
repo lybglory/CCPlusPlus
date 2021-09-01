5a02cc58653e469bbc3a81d844c109ad14657709
小斌仔 <lybglory@outlook.com>
2021-09-01 19:04:58 +0800

    update

--- 03_CppCoreProgramming/06_Inheritance/01对象模型/20210825对象模型.cpp
+++ 03_CppCoreProgramming/06_Inheritance/01对象模型/20210825对象模型.cpp
@@ -2,24 +2,58 @@
 #include <iostream>
 #include <string>
 using namespace std;
-class Base {
+class People {
 public:
+	People() {
+		cout << "People默认构造" << endl;
+	}
+	~People() {
+		cout << "People析构" << endl;
+	}
 	int m_member;
 	int m_sex;
 private:
 	int m_age;
 };
 
-class Student:public Base
+class Student:public People
 {
 public:
+	Student() {
+		cout << "Student默认构造" << endl;
+	}
+	~Student() {
+		cout << "Student析构" << endl;
+	}
 	int m_Id;
-
-private:
-
 };
+
+class Worker :public People
+{
+public:
+	Worker() {
+		cout << "Worker默认构造" << endl;
+	}
+	~Worker() {
+		cout << "Worker析构" << endl;
+	}
+	int m_salary;
+};
+
 class Empty {};
-void main() {
-	cout << "sizeof(Student)=" << sizeof(Student) << endl;
+void test01() {
 	cout << "sizeof(Empty)=" << sizeof(Empty) << endl;
+	cout << "sizeof(People)=" << sizeof(People) << endl;
+	cout << "sizeof(Student)=" << sizeof(Student) << endl;
+	cout << "sizeof(Worker)=" << sizeof(Worker) << endl;
+}
+
+void test02() {
+	People p1;
+	//Student s1;
+	//Worker w1;
+}
+void main() {
+	//test01();
+	test02();
 }
